ea30c3e122450117a2a5f2bff2d2a683240d3e60
philipp.noertersheuser@kdegmbh.onmicrosoft.com <philipp.noertersheuser@kdegmbh.onmicrosoft.com>
2020-04-18 22:24:22 +0200

    Documentation

--- PresetManager/PresetManager.py
+++ PresetManager/PresetManager.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+"""CPreset Manager Main.
+
+Main startup to run the Prese Manager
+
+Classes:
+    controller: main classes handling the program and dataflow
+
+Functions
+
+Todo:
+    * Refactoring to hve cleaner structure
+
+@author:         Philipp Noertersheuser
+@GIT Repository: https://github.com/dimentorium/PresetManager
+@License
+"""
+
+#======================== Imorts Section ====================#
 from tkinter import *
 from tkinter.ttk import *
 from tkinter import simpledialog, filedialog
@@ -10,11 +29,50 @@ import reaper.preset as rp
 import reapy
 import core.ui as ui
 import core.items as items
-
-import copy
+#============================================================#
 
 class main_view():
+    """Main View Class.
+
+    Main Windows built with ttk and all controller functions
+
+    Methods
+    -------
+        init: init class and create window
+        search: get search string
+        save_preset: save a preset from a Reaper track
+        load_preset: load a preset to a Reaper track
+        load_database: load database from file
+        save_database: save database to file
+        new_database: create empty database
+        select_item: event when item in preset tree is selected
+        update_info: update info tree
+        update_list: update preset tree
+        update_ui: update UI element status
+
+    Properties
+    ----------
+        preset_list: list of all presets, database
+        _search_filter: string that is used for searching
+        _selected_item: item selected in preset tree
+        root: root windo for all elements
+        btn_new_database: button to create new database
+        btn_load_database: button for loading database
+        btn_save_database: button for saving database
+        btn_load_preset: button for loading preset
+        btn_save_preset: button for saving preset
+        _entry_text: variable for search string
+        search_box: entry field for search string
+        presettree: tree for displaying database
+        presetinfo: tree for displaying selected item information
+    """
+    
     def __init__(self):
+        """Init.
+
+        Initialize class properties.
+        Builds window and links function calls
+        """
         self.preset_list = {}
         self._search_filter = ""
         self._selected_item = None
@@ -92,17 +150,34 @@ class main_view():
         self.root.wm_attributes("-topmost", 1)
         self.root.mainloop()
 
+#============================================================#
+#=================== Functions ==============================#
+
     def search(self, *args):
+        """search.
+
+        Sets the filter from the search entry field and updates UI
+        """
+        #get string from entry field and use for filtering database
         self._search_filter = self._entry_text.get()
         self.update_list()
         self.update_ui()        
     
     def save_preset(self):
+        """save preset.
+
+        Saves the preset from the selected Reper Track and updates UI.
+        If no track is selected, the user is warned.
+        """
+        #select project and check if a track is selected
         project = reapy.Project()
         if project.n_selected_tracks > 0:
+            #open preset dialog and configure setting
             cancelled, result = ui.save_preset_dialog(self.root)
             if not cancelled:
+                #get chunk from selected track
                 newpreset = rp.save(result[0])
+                #create new item and add it to database
                 vsti_item = items.vstipreset(result[0], newpreset, result[1])
                 self.preset_list[result[0]] = vsti_item
                 self.update_list()
@@ -113,29 +188,57 @@ class main_view():
 
         self.update_ui()
             
-
-
     def load_preset(self):
+        """load.
+
+        Loads selected preset to selected or new track in Reaper
+        """
+        #call load function from selected item
         self._selected_item.load()
         self.update_ui()
 
     def save_database(self):
+        """save database.
+
+        Saves the complete database of presets into a binary file with pickle.
+        User is asked for folder where to save
+        """
+        #open file dialog to select database file and pickle data to file
         filename = filedialog.asksaveasfilename(title = "Select file",filetypes = (("database","*.bin"),("all files","*.*")))
         pickle.dump(self.preset_list, open(filename,"wb"))
         self.update_ui()
 
     def load_database(self):
+        """load database.
+
+        Loads a database of presets from a binary file with pickle.
+        User is asked for file where to load from
+        """
+        #open file dialog to select database file and pickle data from file
         filename = filedialog.askopenfilename(title = "Select file",filetypes = (("database","*.bin"),("all files","*.*")))
         self.preset_list = pickle.load( open( filename, "rb" ) )
         self.update_list()
         self.update_ui()
 
     def new_database(self):
+        """new database.
+
+        Creates empty database
+        """
         self.preset_list = {}
         self.update_list()
         self.update_ui()
 
     def select_item(self, evt):
+        """select item.
+
+        Event that is called when user clicks on the treeview with the preset
+
+        Parameters
+        ----------
+            evt: event armgument passed from tkinter
+        """
+        #get selected item from tree and select from list
         selected_item = self.presettree.item(self.presettree.focus())
         index = selected_item["text"]
         self._selected_item = self.preset_list[index]
@@ -144,33 +247,56 @@ class main_view():
         self.update_ui()
 
     def update_info(self):
+        """update info.
+
+        Updates info tree with selected item
+        """
+        #clear all entries in info tree
         self.presetinfo.delete(*self.presetinfo.get_children())
+        #check if an item in preset tree is selected
         if self._selected_item != None:
+            #loop over properties from selected item and display them in info tree
             for key, value in self._selected_item.properties.items():
                 self.presetinfo.insert("", END, text=key, values=(value,))
 
     def update_list(self):
+        """update list.
+
+        Updates preset list from database array
+        """
+        #clear all entries in preset tree
         self.presettree.delete(*self.presettree.get_children())
+
+        #loop over all items in list
         for preset in self.preset_list:
+            #call filter function from item to check if it should be shown
             show = True
             if self._search_filter != "":
                 show = self.preset_list[preset].check_filter(self._search_filter)
 
+            #show item based on filter function result
             if show:
                 self.presettree.insert("", END, 
                                         text=self.preset_list[preset].preset_name, 
                                         values=(self.preset_list[preset].chunk.plugin_name,))
 
     def update_ui(self):
+        """update UI.
+
+        Updates status of controls based on current selectios
+        """
+
+        #check if an item in tree is selected, if not reset internal variable
         if len(self.presettree.selection()) == 0:
             self._selected_item = None
-        #======================== Update Save Database Button ====================
+
+        #enabe or disable save button if there is something in the database
         if len(self.preset_list) == 0:
             self.btn_save_database['state'] = DISABLED
         else:
             self.btn_save_database['state'] = NORMAL
 
-        #======================== Update Load Preset Button ====================
+        #enable or disable load button if an item is selected
         if self._selected_item == None:
             self.btn_load_preset['state'] = DISABLED
         else:
