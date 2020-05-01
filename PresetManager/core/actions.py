bcab9109accc24816132c0a28016272947006db3
philipp.noertersheuser@kdegmbh.onmicrosoft.com <philipp.noertersheuser@kdegmbh.onmicrosoft.com>
2020-05-01 21:31:15 +0200

    Documentation

--- PresetManager/core/actions.py
+++ PresetManager/core/actions.py
@@ -1,19 +1,36 @@
+# -*- coding: utf-8 -*-
+"""Actions module.
+
+Contains functions that are called by UI or command server
+
+Functions
+
+Todo:
+
+@author:         Philipp Noertersheuser
+@GIT Repository: https://github.com/dimentorium/PresetManager
+@License
+"""
+
 import os
 import logging
 import core.globals as glob
-
 import core.items as items
 
 def save_preset():
-        """Save preset.
+    """Save preset.
 
-        Saves the preset from the selected Reaper Track and updates UI.
-        If no track is selected, the user is warned.
-        """
-        newitem = items.vstipreset()
-        if newitem.save():
-            logging.debug('Saving Preset: ' + newitem.preset_name)
+    Saves the preset from the selected Reaper Track and updates UI.
+    If no track is selected, the user is warned.
+    """
+    newitem = items.vstipreset()
+    if newitem.save():
+        logging.debug('Saving Preset: ' + newitem.preset_name)
 
 def show():
+    """Show.
+
+    Brings Preset Manager Window to front
+    """
     glob.root.wm_attributes("-topmost", 1)
     glob.root.wm_attributes("-topmost", 0)
