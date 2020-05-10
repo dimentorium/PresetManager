1a7cdcc5d87b794f40d8e77c16fdba53bea28eb7
philipp.noertersheuser@kdegmbh.onmicrosoft.com <philipp.noertersheuser@kdegmbh.onmicrosoft.com>
2020-05-10 12:39:56 +0200

    Cleanup and doc

--- PresetManager/core/globals.py
+++ PresetManager/core/globals.py
@@ -1,6 +1,17 @@
-import os
+#-*- coding: utf-8 -*-
+"""Globals module.
 
+Module for handling global references. Is used as a singleton
+Todo:
 
+@author:         Philipp Noertersheuser
+@GIT Repository: https://github.com/dimentorium/PresetManager
+@License
+"""
+
+#reference to root and view
 root = None
-application_folder = ""
 main_window = None
+
+#reference to folder where application is started from. Either script or exe
+application_folder = ""
