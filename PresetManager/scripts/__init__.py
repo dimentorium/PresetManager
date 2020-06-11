4122ed08b6ac25bad3cc8506a508c8508701d545
$raipt <philipp.noertersheuser@gmx.de>
2020-06-11 14:09:11 +0200

    Script folder

---
+++ PresetManager/scripts/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""Reaper Scripts Package.
+
+This package contains scripts that can be used in Reaper to control the Preset Manager
+
+Modules:
+
+Todo:
+    *
+
+@author:         Philipp Noertersheuser
+@GIT Repository: https://github.com/dimentorium/PresetManager
+@License
+"""
